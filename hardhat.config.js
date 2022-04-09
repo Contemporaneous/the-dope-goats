cbac5928aba18a6a0b41df09a4c6045837f8ebda
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-09 11:36:03 +1000

    Update to Mumbai and Correct NFT format

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,11 @@ require("dotenv").config();
  module.exports = {
   solidity: "0.8.4",
   networks: {
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80001
+    },
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
